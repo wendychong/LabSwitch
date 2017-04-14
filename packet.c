ff7913d4983b9e20fb1442b0f0a55ae4770d32c6
Micah Mynatt <mynatt@hawaii.edu>
2017-04-13 16:37:42 -1000

    added tabs (lol)

--- packet.c
+++ packet.c
@@ -18,61 +18,61 @@
 
 void packet_send(struct net_port *port, struct packet *p)
 {
-char msg[PAYLOAD_MAX+4];
-int i;
+	char msg[PAYLOAD_MAX+4];
+	int i;
 
-msg[0] = (char) p->src;
-msg[1] = (char) p->dst;
-msg[2] = (char) p->type;
-msg[3] = (char) p->length;
-for (i=0; i<p->length; i++) {
-	msg[i+4] = p->payload[i];
-}
+	msg[0] = (char) p->src;
+	msg[1] = (char) p->dst;
+	msg[2] = (char) p->type;
+	msg[3] = (char) p->length;
+	for (i=0; i<p->length; i++) {
+		msg[i+4] = p->payload[i];
+	}
 
-if (port->type == PIPE) {
-	write(port->send_fd, msg, p->length+4);
-}
-else if (port->type == SOCKET) {
-	send(port->send_fd, msg, p->length+4, 0);
-}
-/*printf("PACKET SEND, src=%d dst=%d p-src=%d p-dst=%d\n",
-		(int) msg[0],
-		(int) msg[1],
-		(int) p->src,
-		(int) p->dst);*/
+	if (port->type == PIPE) {
+		write(port->send_fd, msg, p->length+4);
+	}
+	else if (port->type == SOCKET) {
+		send(port->send_fd, msg, p->length+4, 0);
+	}
+	/*printf("PACKET SEND, src=%d dst=%d p-src=%d p-dst=%d\n",
+			(int) msg[0],
+			(int) msg[1],
+			(int) p->src,
+			(int) p->dst);*/
 
-return;
+	return;
 }
 
 int packet_recv(struct net_port *port, struct packet *p)
 {
-char msg[PAYLOAD_MAX+4];
-int n;
-int i;
+	char msg[PAYLOAD_MAX+4];
+	int n;
+	int i;
 
-if (port->type == PIPE) {
-	n = read(port->recv_fd, msg, PAYLOAD_MAX+4);
-}
-else if (port->type == SOCKET) {
-	n = recv(port->recv_fd, msg, PAYLOAD_MAX+4, 0);
-}
-if (n>0) {
-	p->src = (char) msg[0];
-	p->dst = (char) msg[1];
-	p->type = (char) msg[2];
-	p->length = (int) msg[3];
-	for (i=0; i<p->length; i++) {
-		p->payload[i] = msg[i+4];
+	if (port->type == PIPE) {
+		n = read(port->recv_fd, msg, PAYLOAD_MAX+4);
+	}
+	else if (port->type == SOCKET) {
+		n = recv(port->recv_fd, msg, PAYLOAD_MAX+4, 0);
+	}
+	if (n>0) {
+		p->src = (char) msg[0];
+		p->dst = (char) msg[1];
+		p->type = (char) msg[2];
+		p->length = (int) msg[3];
+		for (i=0; i<p->length; i++) {
+			p->payload[i] = msg[i+4];
+		}
+
+	/*printf("PACKET RECV, src=%d dst=%d p-src=%d p-dst=%d\n",
+		(int) msg[0],
+		(int) msg[1],
+		(int) p->src,
+		(int) p->dst);*/
 	}
 
-/*printf("PACKET RECV, src=%d dst=%d p-src=%d p-dst=%d\n",
-	(int) msg[0],
-	(int) msg[1],
-	(int) p->src,
-	(int) p->dst);*/
-}
-
-return(n);
+	return(n);
 }
 
 
